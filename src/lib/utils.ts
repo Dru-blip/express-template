d9eaaeebcd5570331dd793e6b0da58a1f40adc5d
Dru-blip <62353412+Dru-blip@users.noreply.github.com>
2024-06-16 16:01:27 +0530

    updated project structure

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "express";
-
-
-
-export const send=(res:Response,status:number,msg:string,payload:object,err?:string)=>{
-    res.status(status).send({status,msg,data:payload,err})
-}
